3d5ed66979d5742fe83ee294ffd9ea5a6429d707
Craig McDonald <craig@mcdonald.codes>
2022-03-31 18:15:57 +0100

    Basic CLI parsing added

--- main.go
+++ main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 func main() {
+
+	timeout := flag.Int("t", 5_000, "timeout value in millis")
+	flag.Parse()
+
+	dirtyNodes := flag.Args()
+
+	fmt.Println("Nodes supplied:", dirtyNodes)
+	fmt.Printf("Timeout %dms\n", *timeout)
 }
