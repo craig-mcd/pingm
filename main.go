e2e3612b77a7a7ebc3b9da96fd55851a5b6b5e24
Craig McDonald <craig@mcdonald.codes>
2022-04-01 11:18:27 +0100

    Basic functionality added for pinging hosts concurrently

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"sync"
+	"time"
+
+	"github.com/go-ping/ping"
 )
 
 func main() {
 
-	timeout := flag.Int("t", 5_000, "timeout value in millis")
+	timeout := flag.Int64("t", 5_000, "timeout value in millis")
 	flag.Parse()
 
 	dirtyNodes := flag.Args()
@@ -23,6 +27,54 @@ func main() {
 		fmt.Println("Invalid nodes:", invalid)
 	}
 
-	fmt.Println("Nodes supplied:", nodes)
-	fmt.Printf("Timeout %dms\n", *timeout)
+	timeoutDuration := time.Duration(*timeout) * time.Millisecond
+	var wg sync.WaitGroup
+
+	for {
+		// used to force each iteration to wait for the timeout
+		// TODO check if this is idiomatic Go
+		wg.Add(1)
+		go func() {
+			time.Sleep(timeoutDuration)
+			wg.Done()
+		}()
+
+		for _, node := range nodes {
+
+			wg.Add(1)
+
+			go func(node string) {
+
+				defer wg.Done()
+
+				pinger, err := ping.NewPinger(node)
+
+				if err != nil {
+					panic(err)
+				}
+
+				pinger.Count = 1
+				pinger.SetPrivileged(true)
+				pinger.Timeout = timeoutDuration
+				err = pinger.Run()
+
+				if err == nil {
+
+					stats := pinger.Statistics()
+
+					if stats.PacketsRecv > 0 {
+						fmt.Printf("%-30s %dms\n", node, *&stats.AvgRtt/time.Millisecond)
+					} else {
+						fmt.Printf("%-30s timed out\n", node)
+					}
+				} else {
+					fmt.Printf("%-30s %s\n", node, err.Error())
+				}
+
+			}(node)
+		}
+
+		wg.Wait()
+		fmt.Println()
+	}
 }
