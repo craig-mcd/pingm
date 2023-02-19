af56e8e4076a94d08be7271f600e9428466fe476
Craig McDonald <craig@mcdonald.codes>
2023-02-19 13:11:52 +0000

    Time added to each batch too high, 100ms now 10ms

--- main.go
+++ main.go
@@ -87,8 +87,8 @@ func main() {
 		// used to force each iteration to wait for the timeout
 		wg.Add(1)
 		go func() {
-			// 100ms added to the batch timeout so not the exact value of the ping timeout
-			time.Sleep(timeoutDuration + 100*time.Millisecond)
+			// 10ms added to the batch timeout so not the exact value of the ping timeout
+			time.Sleep(timeoutDuration + 10*time.Millisecond)
 			wg.Done()
 		}()
 
